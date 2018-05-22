543e69f803aaeebc5ab9d0c14d5cd3941fde3969
David Gee <david.john.gee@gmail.com>
2018-05-22 17:22:29 +0100

    Updated main

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"syscall"
 
-	"github.com/Sirupsen/logrus"
 	auth_pb "github.com/arsonistgopher/junos-gnmi-telem-testclient/authentication"
 	gnmipb "github.com/arsonistgopher/junos-gnmi-telem-testclient/proto/gnmi"
+	"golang.org/x/crypto/ssh/terminal"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
@@ -24,7 +29,7 @@ import (
 // "/junos/task-memory-information"
 // "/junos/system/linecard/firewall/"
 // "/interfaces/interface[name='fxp0']/"
-const SUBSCRIPTIONPATH = "/interfaces/"
+// const SUBSCRIPTIONPATH = "/interfaces/"
 
 func processGNMIResponse(resp *gnmipb.SubscribeResponse) {
 	if notif := resp.GetUpdate(); notif != nil {
@@ -83,40 +88,98 @@ func subSendAndReceiveGNMI(conn *grpc.ClientConn, req *gnmipb.SubscribeRequest) 
 
 func main() {
 
+	log.Println("-----------------------------------")
+	log.Println("Junos gNMI Telemetry Test Tool")
+	log.Println("-----------------------------------")
+	log.Print("Run the app with -h for options\n\n")
+
+	// Parse flags
+	var host = flag.String("host", "127.0.0.1", "Set host to IP address or FQDN DNS record")
+	var subscription = flag.String("subscription", "/interfaces/", "Set subscription to path")
+	var user = flag.String("user", "testuser", "Set to username")
+	var port = flag.String("port", "50051", "Set to Server Port")
+	var cid = flag.String("cid", "1", "Set to Client ID")
+	var certDir = flag.String("certdir", "", "Directory with clientCert.crt, clientKey.crt, CA.crt")
+	flag.Parse()
+
 	// Set host
-	hostandport := "vmx02.corepipe.co.uk:50051"
+	hostandport := *host + ":" + *port
+
+	// Grab password
+	fmt.Print("Enter Password: \n")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatalf("Error reading password: %v", err)
+	}
+	password := string(bytePassword)
 
 	// gRPC options
 	var opts []grpc.DialOption
+
+	// Are we going to run with TLS?
+	runningWithTLS := false
+	if *certDir != "" {
+		runningWithTLS = true
+	}
+
 	opts = append(opts, grpc.WithInitialWindowSize(524288))
 
-	creds, err := credentials.NewClientTLSFromFile("certs/vmx02.corepipe.co.uk.crt", "")
-	if err != nil {
-		logrus.Fatalf("Could not load certFile: %v", err)
+	// If we're running with TLS
+	if runningWithTLS {
+
+		// Grab x509 cert/key for client
+		cert, err := tls.LoadX509KeyPair(fmt.Sprintf("%s/client.crt", *certDir), fmt.Sprintf("%s/client.key", *certDir))
+
+		if err != nil {
+			log.Fatalf("Could not load certFile: %v", err)
+		}
+		// Create certPool for CA
+		certPool := x509.NewCertPool()
+
+		// Get CA
+		ca, err := ioutil.ReadFile(fmt.Sprintf("%s/CA.crt", *certDir))
+		if err != nil {
+			log.Fatalf("could not read ca certificate: %s", err)
+		}
+
+		// Append CA cert to pool
+		if ok := certPool.AppendCertsFromPEM(ca); !ok {
+			log.Fatal("Failed to append client certs")
+		}
+
+		// build creds
+		creds := credentials.NewTLS(&tls.Config{
+			RootCAs:      certPool,
+			Certificates: []tls.Certificate{cert},
+			ServerName:   *host,
+		})
+
+		if err != nil {
+			log.Fatalf("Could not load clientCert: %v", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else { // Else we're not running with TLS
+		opts = append(opts, grpc.WithInsecure())
 	}
-	opts = append(opts, grpc.WithTransportCredentials(creds))
 
 	conn, err := grpc.Dial(hostandport, opts...)
 	if err != nil {
-		logrus.Fatalf("Error opening grpc.Dial(): %v", err)
+		log.Fatalf("Error opening grpc.Dial(): %v", err)
 	}
 	// lazy close
 	defer conn.Close()
 
 	// Check for auth
 	l := auth_pb.NewLoginClient(conn)
-	dat, err := l.LoginCheck(context.Background(), &auth_pb.LoginRequest{UserName: "jet", Password: "Passw0rd", ClientId: "42"})
+	dat, err := l.LoginCheck(context.Background(), &auth_pb.LoginRequest{UserName: *user, Password: password, ClientId: *cid})
 
 	if err != nil {
-		logrus.Fatalf("Could not login: %v", err)
+		log.Fatalf("Could not login: %v", err)
 	}
 
 	if dat.Result == false {
-		logrus.Fatalf("LoginCheck failed\n")
-	}
-
-	if err != nil {
-		logrus.Fatalf("%v", err)
+		log.Fatalf("LoginCheck failed\n")
 	}
 
 	s := &gnmipb.SubscribeRequest_Subscribe{
@@ -127,7 +190,7 @@ func main() {
 		},
 	}
 
-	gpath, err := xpathToGNMIpath(SUBSCRIPTIONPATH)
+	gpath, err := xpathToGNMIpath(*subscription)
 	if err != nil {
 		log.Fatalf("%v\n", err)
 	}
@@ -151,9 +214,8 @@ func main() {
 	subSendAndReceiveGNMI(conn, req)
 
 	if err != nil {
-		logrus.Fatalf("%v", err.Error())
+		log.Fatalf("%v", err.Error())
 	}
 
-	// /*
 	fmt.Println("Exit")
 }
